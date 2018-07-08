7a2729148b4a43685d7a7a73183a00058de858b5
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-08 23:08:17 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -3,8 +3,6 @@ i = 0
 new_array = []
   while i < array.length
     new_array[i] = yield(array[i])
-    #new_array[i] = array[i].upcase
-    #new_array[i] = array[i].split(" ").first
     i+=1
   end
   new_array
