9be925a8e696635614fa60231347093b4ceacb12
Jessica Castaneda <pulsar.ppz70m@gmail.com>
2018-07-08 23:01:03 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,10 +1,12 @@
 def my_collect(array)
 i = 0
+new_array = []
   while i < array.length
     yield(array[i])
+    new_array[i] = array[i].upcase
     i+=1
   end
-  array
+  new_array
 end
 
 #my_collect(array1) do
